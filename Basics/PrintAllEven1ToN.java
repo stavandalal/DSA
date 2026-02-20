e2f81d3387def80795af2d56a4e944441ed534c9
Stavan Dalal <58332350+stavandalal@users.noreply.github.com>
2026-02-20 22:59:00 +0530

    Add program to print all even numbers from 1 to N
    Used for loop and modulus operator to print even numbers.
    Also practiced loop control and iteration concepts.

---
+++ Basics/PrintAllEven1ToN.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class PrintAllEven1ToN {
+    public static void main(String[] args) {
+        int num = 55;
+        for(int i=2;i<=num; i= i+2){
+            System.out.println(i);
+        }
+    }
+}
+
