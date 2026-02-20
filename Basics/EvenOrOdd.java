47ee4f6f2bc1a5229421284719e8721b1c01a030
Stavan Dalal <58332350+stavandalal@users.noreply.github.com>
2026-02-20 22:57:55 +0530

    Add even or odd number checker in Java
    Program checks whether a number is even or odd using modulus operator.
    Practiced conditional statements and basic logic building.

---
+++ Basics/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int num = 44;
+            if(num % 2 ==0){
+                System.out.println(num+ " is Even");
+            }else {
+                System.out.println(num+" is Odd");
+            }
+    }
+}
+
