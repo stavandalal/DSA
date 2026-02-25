aa68264e2f1118a11f0d593dee24a86e5ec15068
Stavan Dalal <58332350+stavandalal@users.noreply.github.com>
2026-02-25 12:10:48 +0530

    Implement factorial calculation in Java
    Created a program to find factorial of a number.
    Used iterative approach (for loop) to multiply numbers from 1 to n.
    Program accepts input and displays the factorial output.

---
+++ Basics/Factorial.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        int originalNum = n;
+        int fact = 1;
+        for(int i=1; i<=n; i++){
+            fact = fact * i;
+        }
+        System.out.println("The Factorial of "+ originalNum +" is "+fact);
+    }
+}
